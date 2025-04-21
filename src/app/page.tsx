c4e669f433fa0a1fc67e99d32ee2d50bac8a1a94
harada06 <yuna.harada0610@gmail.com>
2025-04-21 19:20:27 +0900

    '眠い'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,49 @@
 'use client';
 
+import { useState } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
+  const [board, setBoard] = useState([
+    [1, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const [turnColor, setTurnColor] = useState(1);
+  const clickHandler = (x: number, y: number) => {
+    console.log(x, y);
+    const newBoard = structuredClone(board);
+    newBoard[y][x] = turnColor;
+
+    setBoard(newBoard);
+    if (turnColor === 1) {
+      setTurnColor(2);
+    } else {
+      setTurnColor(1);
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.board}>a</div>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
+              {board[y][x] !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : '#FFF' }}
+                />
+              )}
+            </div>
+          )),
+        )}
+      </div>
     </div>
   );
 }
